778df1bc535a7098445e956b1b52f325dc799c36
Drew Dodson <drewdodson@cox.net>
2020-01-15 14:27:02 -0800

    Completed 'Session 03'

---
+++ Session 03/Lab-Operators.playground/Contents.swift
@@ -0,0 +1,41 @@
+// Session 03 - Lab-Operators.playground
+// CSE-41201, 145850, Winter 2020
+// Robert Dodson
+
+import UIKit
+
+let favoritePerson = "Luke"
+
+var shoeSize = 8
+shoeSize = 9 //Reassign shoeSize to Int 9
+
+var opponentScore = 3 * 8
+var myScore = 100 / 4
+var totalScore = opponentScore + myScore
+
+myScore = myScore + 3
+
+let totalDistance = 3.9
+var distanceTraveled = 1.2
+var remainingDistance = totalDistance - distanceTraveled
+
+let x: Double = 51
+let y: Double = 4
+
+let z = x / y
+
+
+myScore += 3
+myScore -= 5
+myScore *= 2
+myScore /= 2
+
+var x2 = 2
+var y2 = 3
+var z2 = 4
+x2 + y2 * z2
+(x2 + y2) * z2
+
+let x3 = 3
+let y3 = 0.1415927
+let z3 = Double(x3) + y3
