fc5ac63eb61f56abb1b620e489ac3685c997358a
Drew Dodson <drewdodson@cox.net>
2020-01-23 09:50:14 -0800

    Completed 'Session 05'

---
+++ Session 05/GettingStarted/GettingStarted/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GettingStarted
+//
+//  Created by Drew Dodson on 1/23/20.
+//  Copyright © 2020 Arcalla.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
