fcc7d59a490205d7b962b869144812563abd1ab9
Drew Dodson <drewdodson@cox.net>
2020-03-03 14:50:50 -0800

    Completed Apple Pie project

---
+++ Session 21/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Robert Dodson on 3/3/20.
+//  Copyright © 2020 Arcalla.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+
+
+
+    //Variables to play the game including words and bad choices allowed
+    var listOfWords = ["house", "vehicle", "moose", "program", "table", "flower"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var currentGame: Game!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+
+
+
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+
+
+
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = true
+        }
+    }
+
+
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+
+
+
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+
+
+
+    //Button pressed method
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        //updateUI()
+        updateGameState()
+    }
+    
+
+
+
+
+// FINAL } IN THE CLASS
+}
